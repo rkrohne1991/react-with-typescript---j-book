5345f49b03a5b76aa7cb097116e4f6f4ab701e6b
Rafal <rafal.krohne@hotmail.co.uk>
2022-03-09 11:37:56 +0100

    Copying esbuild.wasm into public folder,  Initializing ESBuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import * as esbuild from "esbuild-wasm";
+import { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
+
+  const test = "Test";
+
+  const startService = async () => {
+    const service = await esbuild
+      .initialize({
+        worker: true,
+        wasmURL: "/esbuild.wasm",
+      })
+      .then(() => {
+        esbuild.transform(test).then((result) => {
+          console.log(result);
+        });
+      });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
 
   const onClick = () => {
     console.log(input);
