b588fb8a9579ba0d64d8c685d51aaf21cb8b1b73
Rafal <rafal.krohne@hotmail.co.uk>
2022-03-09 13:43:52 +0100

    Dive on Bundling, Dynamically Fetching Modules

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import * as esbuild from "esbuild-wasm";
 import { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+
 const App = () => {
   const ref = useRef<any>();
   const [input, setInput] = useState("");
@@ -23,12 +25,21 @@ const App = () => {
   const onClick = async () => {
     if (!ref.current) return;
 
-    const result = await esbuild.transform(input, {
-      loader: "jsx",
-      target: "es2015",
+    // const result = await esbuild.transform(input, {
+    //   loader: "jsx",
+    //   target: "es2015",
+    // });
+
+    const result = await esbuild.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
     });
 
-    setCode(result.code);
+    // console.log(result);
+
+    setCode(result.outputFiles[0].text);
   };
 
   return (
