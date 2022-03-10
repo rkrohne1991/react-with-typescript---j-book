d563ef1dc7dfb4bc31415b5aea2bb3bbee1c0313
Rafal <rafal.krohne@hotmail.co.uk>
2022-03-10 14:37:33 +0100

    Loading CSS Files

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -20,18 +20,25 @@ export const fetchPlugin = (inputCode: string) => {
 
         // Check to see if we have already fetched this file
         // and if it is in the cache
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
-
-        // if it is, return it immidately
-        if (cachedResult) return cachedResult;
-
         const { data, request } = await axios.get(args.path);
+        const fileType = args.path.match(/.css$/) ? "css" : "jsx";
+
+        const escaped = data
+          .replace(/\n/g, "")
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+        const contents =
+          fileType === "css"
+            ? `
+            const style = document.createElement("style");
+            style.innerText = "${escaped}";
+            document.head.appendChild(style);
+          `
+            : data;
 
         const result: esbuild.OnLoadResult = {
           loader: "jsx",
-          contents: data,
+          contents: contents,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
 
