632ae8cb804f71480a10e0a6659d528fdd54b640
Rafal <rafal.krohne@hotmail.co.uk>
2022-03-10 10:37:54 +0100

    Using the URL Constructor

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,6 +10,13 @@ export const unpkgPathPlugin = () => {
         if (args.path === "index.js")
           return { path: args.path, namespace: "a" };
 
+        if (args.path.includes("./") || args.path.includes("../")) {
+          return {
+            namespace: "a",
+            path: new URL(args.path, args.importer + "/").href,
+          };
+        }
+
         return {
           namespace: "a",
           path: `https://unpkg.com/${args.path}`,
