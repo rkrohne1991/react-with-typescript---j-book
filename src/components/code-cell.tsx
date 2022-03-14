f62b521de480856b348028ff7a78eb70d6d55674
Rafal <rafal.krohne@hotmail.co.uk>
2022-03-14 12:18:25 +0100

    Draggable Resizing Components

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,31 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import CodeEditor from "./code-editor";
 import Preview from "./preview";
 import bundle from "../bundler/index";
 
-import "bulmaswatch/superhero/bulmaswatch.min.css";
+import Resizable from "./resizable";
 
 const CodeCell = () => {
   const [code, setCode] = useState("");
   const [input, setInput] = useState("");
 
-  const onClick = async () => {
-    const outout = await bundle(input);
-    setCode(outout);
-  };
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
+
+  // const onClick = async () => {
+  //   const output = await bundle(input);
+  //   setCode(output);
+  // };
 
   return (
-    <div>
-      <CodeEditor
-        initialValue="const a = 1;"
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
+    <Resizable direction="vertical">
+      <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue="const a = 1;"
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
+        <Preview code={code} />
       </div>
-      <Preview code={code} />
-    </div>
+    </Resizable>
   );
 };
 
