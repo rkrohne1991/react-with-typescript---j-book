01619116fba58da9938c5d0c64c96a7ff3edcf4f
Rafal <rafal.krohne@hotmail.co.uk>
2022-03-15 13:12:20 +0100

    Design patterns for redux with typescript and simplifying state updates with Immer

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
-import ReactDOM from "react-dom";
-
-import CodeCell from "./components/code-cell";
-import TextEditor from "./components/text-editor";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
+
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+// import CodeCell from "./components/code-cell";
+import TextEditor from "./components/text-editor";
+import { store } from "./state";
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-      {/* <CodeCell /> */}
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+        {/* <CodeCell /> */}
+      </div>
+    </Provider>
   );
 };
 
---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,30 @@
+import { ActionType } from "../action-types";
+import {
+  DeleteCellAction,
+  Direction,
+  InsertCellBeforeAction,
+  MoveCellAction,
+  UpdateCellAction,
+} from "../actions";
+import { CellTypes } from "../cell";
+
+export const updateCell = (id: string, content: string): UpdateCellAction => ({
+  type: ActionType.UPDATE_CELL,
+  payload: { id, content },
+});
+export const deleteCell = (id: string): DeleteCellAction => ({
+  type: ActionType.DELETE_CELL,
+  payload: id,
+});
+export const moveCell = (id: string, direction: Direction): MoveCellAction => ({
+  type: ActionType.MOVE_CELL,
+  payload: { id, direction },
+});
+
+export const insertCellBefore = (
+  id: string,
+  cellType: CellTypes
+): InsertCellBeforeAction => ({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: { id, type: cellType },
+});
