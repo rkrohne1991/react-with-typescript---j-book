62e01249bb7803cae587e9afb45d102825e318d6
Rafal <rafal.krohne@hotmail.co.uk>
2022-03-18 10:56:37 +0100

    Adding missing router.use, Adding a Persist Middleware, Detecting When to Save, Debouncing save logic

--- jbook/packages/local-client/src/state/store.ts
+++ jbook/packages/local-client/src/state/store.ts
@@ -1,5 +1,10 @@
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
+import { persistMiddleware } from "./middlewares/persist-middleware";
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
+export const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(persistMiddleware, thunk)
+);
